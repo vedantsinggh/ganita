6e67fdb9233b575aa83dab17a9d56a11f7caaa01
Vedant Singh <vedantsinggh@gmail.com>
2024-05-14 01:22:26 +0530

    implement custom stack for ADD operation

--- lexer.cpp
+++ lexer.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
 #include <fstream>
-#include <cassert>
+#include <assert.h>
+#include <vector>
+#include <string>
 
-#define print(x) std::cout<<x
+#define STACK_IMPLEMENTATION
+#include "stack.h"
+
+#define printp(x) std::cout<<x
 #define println(x) std::cout<<x<<std::endl
-#define printer(x) std::cerr<<x<<std::endl
+#define printerr(x) std::cerr<<x<<std::endl
 
-int count = 0;
+OP tokenize(std::string token){
+	int tokenValue;
+	OP_TYPE type = command;
+	      if (token == "+"){
+		tokenValue = plus;
+	}else if (token == "-"){
+		tokenValue = minus;
+	}else if (token == "*"){
+		tokenValue = mul;
+	}else if (token == "/"){
+		tokenValue = divide;
+	}else{
+		tokenValue = std::stoi(token);
+		type  = literal;
+	}
+	OP op{
+		.type = type,
+		.value = tokenValue
+	};
 
-std::string* lexLine(std::string line){
-	std::string* tokens = new std::string[1024];	
+	return op;
+}
+
+std::vector<OP> lexLine(std::string line){
+	std::vector<OP> tokens;
 	std::string token;
-	line += " ";
-
+	line+= " ";
 	for (char t: line){
-		if (t == 32 || t == '\t'){
+		if (t == 32 || t == '\t' || t == ',' || t == '(' || t == ')'){
 			if (!token.size() > 0){
 				continue;
 			}
-			tokens[count] = token;
+			tokens.push_back(tokenize(token));
 			token = "";
-			count++;
 			continue;
 		}
 		token += t;
@@ -28,23 +52,53 @@ std::string* lexLine(std::string line){
 	return tokens;
 }
 
+void execute(Stack program){
+	Stack exect;
+	for(int i=0; i < program.count; i++){
+		if (program.ops[i].type == literal){;
+			exect.push(program.ops[i]);
+		}
+		if (program.ops[i].type == command){
+			switch(program.ops[i].value){
+				case plus:
+					{
+						int a = (exect.pop()).value;
+						int b = (exect.pop()).value;
+						OP op{
+							.type =literal,	
+							.value = a+b	
+						};
+						exect.push(op);
+						break;
+					}
+				default:
+					printp("Kuch toh chod diya raju!");
+					break;
+			}	
+		}
+	}
+	exect.print();
+}
+
 int main(int argc, char* argv[]){
 	
 	assert( argc >= 2 );
-	char* filename = argv[1];
+	std::string filename = argv[1];
+	Stack program;
 	
 	std::ifstream file(filename);
 	if (!file.is_open()) { 
-		printer("Could not open file: " << filename); 
+		printerr("Could not open file: " << filename); 
 		return 1; 
 	} 
 
 	std::string line;
 	while(getline(file, line)){
-		std::string* tok = lexLine(line);
-		for(int i = 0; i < count; i++){
-			println(tok[i]);
+		std::vector<OP> tokens = lexLine(line);
+		for(OP token : tokens){
+			program.push(token);
 		}
-		count = 0;
 	}
+	execute(program);
 }
+
